540bc6a0d6255aa40ffba97c6b6333698186824b
Chris von Csefalvay <chris@chrisvoncsefalvay.com>
2025-10-25 09:47:23 -0600

    Add new layers and utility functions for depth estimation model
    - Implemented various neural network layers in `layers.py`, including ResidualConvUnit and FeatureFusionBlock.
    - Introduced utility functions for depth processing in `utils/layers.py`, including transformations and depth error calculations.
    - Added model downloading functionality in `utils/utils.py` to fetch pretrained models if they do not exist.
    - Created a new `__init__.py` to facilitate module imports from the utils package.

---
+++ models/endomust_code/decoders/__init__.py
@@ -0,0 +1,7 @@
+from .intrinsics_decoder import IntrinsicsHead
+from .pose_decoder import PoseDecoder
+from .pose_cnn import PoseCNN
+from .appearance_flow_decoder import TransformDecoder
+from .optical_flow_decoder import PositionDecoder
+from .depth_decoder import DepthDecoder
+from .hr_decoder import HRDepthDecoder
